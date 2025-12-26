d1c24185765e54707db55bd9e96eb38c4279f6eb
Madiweth <madiwet.mb@gmail.com>
2025-12-26 15:55:03 +0100

    Corrections to model.py

--- model/model.py
+++ model/model.py
@@ -2,34 +2,57 @@
 
 '''
 Architecture of a variational autoencoder model.
-Refer to the constructor of class Vae for a list of tunable hyper-parameters.
 '''
 
 from __future__ import print_function
 import json
-
-
 import keras
-from keras.layers import Input, Dense, Lambda, Flatten, Reshape
-from keras.layers import Conv2D, Conv2DTranspose
+from keras import ops, layers
+from keras.layers import Input, Dense, Flatten, Reshape, Conv2D, Conv2DTranspose, Layer
 from keras.models import Model, model_from_json
-#from keras import backend as K
-from keras import metrics
-from keras import ops
+from keras.losses import binary_crossentropy
+from keras.saving import register_keras_serializable
 
-def _sampling(args, latent_dim):
-    epsilon_std = 1.0
+@register_keras_serializable()
+class Sampling(Layer):
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = ops.shape(z_mean)[0]
+        dim = ops.shape(z_mean)[1]
+        epsilon = keras.random.normal(shape=(batch, dim))
+        return z_mean + ops.exp(0.5 * z_log_var) * epsilon
 
-    z_mean, z_log_var = args
-    epsilon = keras.random.normal(shape=(ops.shape(z_mean)[0], latent_dim),
-                              mean=0., stddev=epsilon_std)
-    return z_mean + ops.exp(z_log_var) * epsilon
+@register_keras_serializable()
+class VAELossLayer(Layer):
+    def call(self, inputs):
+        x, x_decoded, z_mean, z_log_var = inputs
+        
+        shape = ops.shape(x)
+        if keras.config.image_data_format() == 'channels_first':
+            n_pixels = shape[2] * shape[3]
+        else:
+            n_pixels = shape[1] * shape[2]
+        n_pixels = ops.cast(n_pixels, 'float32')
+
+        # Flatten for loss calculation
+        x_flat = ops.reshape(x, (shape[0], -1))
+        x_decoded_flat = ops.reshape(x_decoded, (shape[0], -1))
+
+        # Reconstruction Loss
+        xent_loss = n_pixels * binary_crossentropy(x_flat, x_decoded_flat)
+
+        # KL Divergence
+        kl_loss = -0.5 * ops.sum(1 + z_log_var - ops.square(z_mean) - ops.exp(z_log_var), axis=-1)
+
+        # Add loss to the graph
+        self.add_loss(ops.mean(xent_loss + kl_loss))
+        
+        # Return the decoded image so the model output remains standard
+        return x_decoded
 
 class Vae(object):
-    # filters: number of convolutional filters to use
-    # num_conv: convolution kernel size
-    def __init__(self, latent_dim = 64, img_dim = (3, 64, 64), batch_size = 100,
-         intermediate_dim = 1024, filters = 64, num_conv = 3):
+    def __init__(self, latent_dim=64, img_dim=(3, 64, 64), batch_size=100,
+         intermediate_dim=1024, filters=64, num_conv=3):
         self.latent_dim = latent_dim
         self.batch_size = batch_size
         self.intermediate_dim = intermediate_dim
@@ -41,93 +64,46 @@ class Vae(object):
         else:
             self.original_img_size = (self.img_rows, self.img_cols, self.img_chns)
 
-    def _loss_function (self, x, z_mean, z_log_var, x_decoded_mean_squash):
-        if keras.config.image_data_format() == 'channels_first':
-            _, img_chns, img_rows, img_cols = x.shape
-        else:
-            _, img_rows, img_cols, img_chns = x.shape
-
-        flat = Flatten()
-
-        xent_loss = img_rows * img_cols * metrics.binary_crossentropy(
-            flat(x),
-            flat(x_decoded_mean_squash))
-        kl_loss = - 0.5 * ops.sum(1 + z_log_var - ops.square(z_mean) - ops.exp(z_log_var), axis=-1)
-        vae_loss = ops.mean(xent_loss + kl_loss)
-
-    '''
-    Instantiate the VAE, encoder and decoder model.
-    '''
-    def build_model (self, mpath):
+    def build_model(self, mpath):
+        # Config
         img_chns, img_cols, img_rows = self.img_chns, self.img_cols, self.img_rows
-        original_img_size = self.original_img_size
         latent_dim = self.latent_dim
         intermediate_dim = self.intermediate_dim
-        up_dim = img_rows // 2
         filters, num_conv = self.filters, self.num_conv
-        batch_size = self.batch_size
-
-        x = Input(shape=original_img_size, name='input')
-        conv_1 = Conv2D(img_chns,
-                        kernel_size=(2, 2),
-                        padding='same', activation='relu', name='conv_1')(x)
-        conv_2 = Conv2D(filters,
-                        kernel_size=(2, 2),
-                        padding='same', activation='relu', name='conv_2',
-                        strides=(2, 2))(conv_1)
-        conv_3 = Conv2D(filters,
-                        kernel_size=num_conv,
-                        padding='same', activation='relu', name='conv_3',
-                        strides=1)(conv_2)
-        conv_4 = Conv2D(filters,
-                        kernel_size=num_conv,
-                        padding='same', activation='relu', name='conv_4',
-                        strides=1)(conv_3)
+        
+        # Encoder
+        x = Input(shape=self.original_img_size, name='input')
+        
+        # Convolutional Block
+        conv_1 = Conv2D(img_chns, kernel_size=(2, 2), padding='same', activation='relu', name='conv_1')(x)
+        conv_2 = Conv2D(filters, kernel_size=(2, 2), padding='same', activation='relu', name='conv_2', strides=(2, 2))(conv_1)
+        conv_3 = Conv2D(filters, kernel_size=num_conv, padding='same', activation='relu', name='conv_3', strides=1)(conv_2)
+        conv_4 = Conv2D(filters, kernel_size=num_conv, padding='same', activation='relu', name='conv_4', strides=1)(conv_3)
+        
         flat = Flatten(name='flat')(conv_4)
         hidden = Dense(intermediate_dim, activation='relu', name='hidden')(flat)
 
         z_mean = Dense(latent_dim, name='z_mean')(hidden)
         z_log_var = Dense(latent_dim, name='z_log_var')(hidden)
 
-        # note that "output_shape" isn't necessary with the TensorFlow backend
-        # so you could write `Lambda(sampling)([z_mean, z_log_var])`
-        z = Lambda(_sampling, output_shape=(latent_dim,),
-                    arguments={'latent_dim': latent_dim})([z_mean, z_log_var])
+        # Use Custom Sampling Layer
+        z = Sampling(name='sampling')([z_mean, z_log_var])
 
-        # we instantiate these layers separately so as to reuse them later
+        # Decoder
+        up_dim = img_rows // 2
         decoder_hid = Dense(intermediate_dim, activation='relu', name='decoder_hid')
-        decoder_upsample = Dense(filters * up_dim * up_dim, activation='relu',
-                                    name='decoder_upsample')
+        decoder_upsample = Dense(filters * up_dim * up_dim, activation='relu', name='decoder_upsample')
 
         if keras.config.image_data_format() == 'channels_first':
-            output_shape = (batch_size, filters, up_dim, up_dim)
+            output_shape = (self.batch_size, filters, up_dim, up_dim)
         else:
-            output_shape = (batch_size, up_dim, up_dim, filters)
+            output_shape = (self.batch_size, up_dim, up_dim, filters)
 
         decoder_reshape = Reshape(output_shape[1:], name='decoder_reshape')
-        decoder_deconv_1 = Conv2DTranspose(filters,
-                                        kernel_size=num_conv,
-                                        padding='same',
-                                        strides=1,
-                                        activation='relu',
-                                        name='decoder_deconv_1')
-        decoder_deconv_2 = Conv2DTranspose(filters,
-                                        kernel_size=num_conv,
-                                        padding='same',
-                                        strides=1,
-                                        activation='relu',
-                                        name='decoder_deconv_2')
-        decoder_deconv_3_upsamp = Conv2DTranspose(filters,
-                                                kernel_size=(3, 3),
-                                                strides=(2, 2),
-                                                padding='valid',
-                                                activation='relu',
-                                                name='decoder_deconv_3_upsamp')
-        decoder_mean_squash = Conv2D(img_chns,
-                                    kernel_size=2,
-                                    padding='valid',
-                                    activation='sigmoid',
-                                    name='decoder_mean_squash')
+        decoder_deconv_1 = Conv2DTranspose(filters, kernel_size=num_conv, padding='same', strides=1, activation='relu', name='decoder_deconv_1')
+        decoder_deconv_2 = Conv2DTranspose(filters, kernel_size=num_conv, padding='same', strides=1, activation='relu', name='decoder_deconv_2')
+        decoder_deconv_3_upsamp = Conv2DTranspose(filters, kernel_size=(3, 3), strides=(2, 2), padding='valid', activation='relu', name='decoder_deconv_3_upsamp')
+        decoder_mean_squash = Conv2D(img_chns, kernel_size=2, padding='valid', activation='sigmoid', name='decoder_mean_squash')
 
         hid_decoded = decoder_hid(z)
         up_decoded = decoder_upsample(hid_decoded)
@@ -137,40 +113,21 @@ class Vae(object):
         x_decoded_relu = decoder_deconv_3_upsamp(deconv_2_decoded)
         x_decoded_mean_squash = decoder_mean_squash(x_decoded_relu)
 
-        # instantiate VAE model
-        vae = Model(x, x_decoded_mean_squash)
+        final_output = VAELossLayer(name='vae_loss')([x, x_decoded_mean_squash, z_mean, z_log_var])
 
-        # save model as json
+        # Instantiate Model
+        vae = Model(x, final_output)
+        
+        # Compile with loss=None because loss is added inside VAELossLayer
+        vae.compile(optimizer="rmsprop")
+
+        # Save model JSON
         with open(mpath, 'w') as outfile:
             json.dump(vae.to_json(), outfile)
 
-        def make_vae_loss(img_rows, img_cols, z_mean, z_log_var):
-            def vae_loss(x_true, x_pred):
-                # reconstruction loss
-                flat = Flatten()
-
-                xent_loss = img_rows * img_cols * metrics.binary_crossentropy(
-                    flat(x_true),
-                    flat(x_pred),
-                )
-                # KL divergence
-                kl_loss = -0.5 * ops.sum(
-                    1 + z_log_var - ops.square(z_mean) - ops.exp(z_log_var),
-                    axis=-1,
-                )
-                return ops.mean(xent_loss + kl_loss)
-
-            return vae_loss
-
-        loss_fn = make_vae_loss(img_rows, img_cols, z_mean, z_log_var)
-
-        vae.compile(optimizer="rmsprop", loss=loss_fn)
-
-        # We also instantiate the encoder and decoder
-        # encoder: a model to project inputs on the latent space
+        #Sub-models
         encoder = Model(x, z_mean)
-
-        # decoder: an image generator that can sample from the learned distribution
+        
         decoder_input = Input(shape=(latent_dim,))
         _hid_decoded = decoder_hid(decoder_input)
         _up_decoded = decoder_upsample(_hid_decoded)
@@ -183,46 +140,26 @@ class Vae(object):
 
         return vae, encoder, decoder
 
-    def read (self, fn, weights):
+    def read(self, fn, weights):
         with open(fn, 'r') as infile:
-            vae = model_from_json(json.load(infile))
+            json_str = json.load(infile)
+            vae = model_from_json(json_str)
+            
         vae.load_weights(weights)
-
-        # recover hyper-parameters
-        x = vae.get_layer('input').input
-        z_mean = vae.get_layer('z_mean').output
-        hidden = vae.get_layer('hidden').output
-        self.latent_dim = int(z_mean.shape[1])
-        self.intermediate_dim = int(hidden.shape[1])
-        _, self.img_rows, self.img_cols, self.img_chns = x.get_shape().as_list()
-
-        # we still have to supply the loss funciton and compile the model
-        z_log_var = vae.get_layer('z_log_var').output
-        x_decoded_mean_squash = vae.get_layer('decoder_mean_squash').output
-
-        flat = Flatten()
-
-        # Compute VAE loss
-        xent_loss = self.img_rows * self.img_cols * metrics.binary_crossentropy(
-            flat(x),
-            flat(x_decoded_mean_squash))
-        kl_loss = - 0.5 * ops.sum(1 + z_log_var - ops.square(z_mean) - ops.exp(z_log_var), axis=-1)
-        vae_loss = ops.mean(xent_loss + kl_loss)
-        vae.add_loss(vae_loss)
         vae.compile(optimizer='rmsprop')
 
+        # Recover sub-models using get_layer
         encoder = self.recover_encoder(vae)
         decoder = self.recover_decoder(vae)
 
         return vae, encoder, decoder
 
-    def recover_encoder (self, vae):
+    def recover_encoder(self, vae):
         x = vae.get_layer('input').input
         z_mean = vae.get_layer('z_mean').output
-
         return Model(x, z_mean)
 
-    def recover_decoder (self, vae):
+    def recover_decoder(self, vae):
         decoder_hid = vae.get_layer('decoder_hid')
         decoder_upsample = vae.get_layer('decoder_upsample')
         decoder_reshape = vae.get_layer('decoder_reshape')
@@ -241,20 +178,17 @@ class Vae(object):
         _x_decoded_mean_squash = decoder_mean_squash(_x_decoded_relu)
         return Model(decoder_input, _x_decoded_mean_squash)
 
-    def to_image (self, array):
+    def to_image(self, array):
         array = array.reshape(self.img_rows, self.img_cols, self.img_chns)
         array *= 255
         return array.astype('uint8')
 
-    '''
-    Load or instantiate the VAE, encoder and decoder mdoels
-    '''
-    def init_model (self, infile, weights):
+    def init_model(self, infile, weights):
         try:
             vae, encoder, decoder = self.read(infile, weights)
             print('Successfully loaded model: {}'.format(weights))
-        except:
+        except Exception as e:
+            print(f'Loading failed ({e}). Instantiating new models ...')
             vae, encoder, decoder = self.build_model(infile)
-            print('Instantiating new models ...')
 
         return vae, encoder, decoder
