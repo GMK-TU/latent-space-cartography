cbe685ea981882b5116f1492fd94eb6d75b5c942
MBD05 <madiweth.mb@gmail.com>
2025-12-31 18:35:53 +0100

    Correction of packages.json and webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,5 @@
 var path = require('path')
-const { VueLoaderPlugin } = require('vue-loader');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 var webpack = require('webpack')
 
 const mode = process.env.NODE_ENV || 'development';
@@ -14,11 +14,30 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.vue$/, loader: 'vue-loader', options: { loaders: { } } },
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-      { test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, use: [{ loader: 'file-loader', options: { name: '[name].[ext]', publicPath: '/build/' } }] },
-      { test: /\.(png|jpg|gif|svg)$/, loader: 'file-loader', options: { name: '[name].[ext]?[hash]' } }
+      { 
+      test: /\.vue$/, 
+      loader: 'vue-loader' 
+      },
+      { 
+        test: /\.js$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/ 
+      },
+      { 
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader'] 
+      },
+      { 
+        // This rule handles Fonts AND SVGs now
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, 
+        use: [{ loader: 'file-loader', options: { name: '[name].[ext]', publicPath: '/build/' } }] 
+      },
+      { 
+        // FIX: Removed '|svg' from this regex to prevent the conflict
+        test: /\.(png|jpg|gif)$/, 
+        loader: 'file-loader', 
+        options: { name: '[name].[ext]?[hash]' } 
+      }
     ]
   },
   resolve: {
@@ -32,44 +51,42 @@ module.exports = {
   ],
   devServer: {
     historyApiFallback: true,
-    noInfo: true,
-    contentBase: [
-        path.resolve(__dirname, '.'),       // serves /index.html
-        path.resolve(__dirname, './build')  // serves /build/fontawesome.js etc.
+    // 'static' replaces 'contentBase' in Webpack 5
+    static: [
+        { directory: path.resolve(__dirname, '.') },
+        { directory: path.resolve(__dirname, './build') }
     ],
-    watchContentBase: false,
-    publicPath: '/build/',
-
-    proxy: {
-      "/api": {
+    devMiddleware: {
+        publicPath: '/build/',
+    },
+    // FIX: Converted proxy to an Array format to satisfy the schema
+    proxy: [
+      {
+        context: ['/api'],
         target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
-      },
-    }
+      }
+    ]
   },
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
+  // FIX 4: Removed the '#' prefix. Webpack 5 does not support legacy names.
+  devtool: 'eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.watch = false
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.devtool = 'source-map' // FIX 4: Removed '#'
+  
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
+    // FIX 5: Removed webpack.optimize.UglifyJsPlugin.
+    // Webpack 5 minimizes automatically when mode is 'production'.
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })
